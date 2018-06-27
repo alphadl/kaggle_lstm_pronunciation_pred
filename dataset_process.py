bd23dcd306fc087da0e2b1833b21b5e73cbf69b2
DING Liang <liangding.liam@gmail.com>
2018-06-27 23:31:58 +0800

    baseline model commit

--- dataset_process.py
+++ dataset_process.py
@@ -175,3 +175,273 @@ print('"AH0" is represented by:\n',phone_to_1_hot('AH0'))
   0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.
   0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
 """
+
+#----------#convert our entire dataset into two big 3D matrices (tensors)#-----------------#
+##convert our entire dataset into two big 3D matricies(tensors):
+
+MAX_CHAR_SEQ_LEN = max([len(word) for word, _ in phonetic_dict.items()])
+MAX_PHONE_SEQ_LEN = max([max([len(pron.split()) for pron in pronuns])
+                         for _, pronuns in phonetic_dict.items()]
+                        ) + 2  # + 2 to account for the start & end tokens we need to add
+
+
+def dataset_to_1_hot_tensors():
+    char_seqs = []
+    phone_seqs = []
+
+    for word, pronuns in phonetic_dict.items():
+        word_matrix = np.zeros((MAX_CHAR_SEQ_LEN, CHAR_TOKEN_COUNT))
+        for t, char in enumerate(word):
+            word_matrix[t, :] = char_to_1_hot(char)
+        for pronun in pronuns:
+            pronun_matrix = np.zeros((MAX_PHONE_SEQ_LEN, PHONE_TOKEN_COUNT))
+            phones = [START_PHONE_SYM] + pronun.split() + [END_PHONE_SYM]
+            for t, phone in enumerate(phones):
+                pronun_matrix[t, :] = phone_to_1_hot(phone)
+
+            char_seqs.append(word_matrix)
+            phone_seqs.append(pronun_matrix)
+
+    return np.array(char_seqs), np.array(phone_seqs)
+
+
+char_seq_matrix, phone_seq_matrix = dataset_to_1_hot_tensors()
+print('Word Matrix Shape: ', char_seq_matrix.shape)
+print('Pronunciation Matrix Shape: ', phone_seq_matrix.shape)
+
+"""
+Word Matrix Shape:  (5343, 19, 30)
+Pronunciation Matrix Shape:  (5343, 20, 87)
+"""
+
+phone_seq_matrix_decoder_output = np.pad(phone_seq_matrix,((0,0),(0,1),(0,0)), mode='constant')[:,1:,:]
+
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense
+
+
+def baseline_model(hidden_nodes=256):
+    # Shared Components - Encoder
+    char_inputs = Input(shape=(None, CHAR_TOKEN_COUNT))
+    encoder = LSTM(hidden_nodes, return_state=True)
+
+    # Shared Components - Decoder
+    phone_inputs = Input(shape=(None, PHONE_TOKEN_COUNT))
+    decoder = LSTM(hidden_nodes, return_sequences=True, return_state=True)
+    decoder_dense = Dense(PHONE_TOKEN_COUNT, activation='softmax')
+
+    # Training Model
+    _, state_h, state_c = encoder(char_inputs)  # notice encoder outputs are ignored
+    encoder_states = [state_h, state_c]
+    decoder_outputs, _, _ = decoder(phone_inputs, initial_state=encoder_states)
+    phone_prediction = decoder_dense(decoder_outputs)
+
+    training_model = Model([char_inputs, phone_inputs], phone_prediction)
+
+    # Testing Model - Encoder
+    testing_encoder_model = Model(char_inputs, encoder_states)
+
+    # Testing Model - Decoder
+    decoder_state_input_h = Input(shape=(hidden_nodes,))
+    decoder_state_input_c = Input(shape=(hidden_nodes,))
+    decoder_state_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_outputs, decoder_state_h, decoder_state_c = decoder(phone_inputs, initial_state=decoder_state_inputs)
+    decoder_states = [decoder_state_h, decoder_state_c]
+    phone_prediction = decoder_dense(decoder_outputs)
+
+    testing_decoder_model = Model([phone_inputs] + decoder_state_inputs, [phone_prediction] + decoder_states)
+
+    return training_model, testing_encoder_model, testing_decoder_model
+
+
+from sklearn.model_selection import train_test_split
+
+TEST_SIZE = 0.2
+
+(char_input_train, char_input_test,
+ phone_input_train, phone_input_test,
+ phone_output_train, phone_output_test) = train_test_split(
+    char_seq_matrix, phone_seq_matrix, phone_seq_matrix_decoder_output,
+    test_size=TEST_SIZE, random_state=42)
+
+TEST_EXAMPLE_COUNT = char_input_test.shape[0]
+
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+
+def train(model, weights_path, encoder_input, decoder_input, decoder_output):
+    checkpointer = ModelCheckpoint(filepath=weights_path, verbose=1, save_best_only=True)
+    stopper = EarlyStopping(monitor='val_loss', patience=3)
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+    model.fit([encoder_input, decoder_input], decoder_output,
+              batch_size=256,
+              epochs=100,
+              validation_split=0.2,  # Keras will automatically create a validation set for us
+              callbacks=[checkpointer, stopper])
+
+BASELINE_MODEL_WEIGHTS = os.path.join(
+    './input','predict-english-pronunciations-model-weights','baseline_model_weights.hdf5'
+)
+
+training_model, testing_encoder_model, testing_decoder_model = baseline_model()
+if not IS_KAGGLE:
+    train(training_model, BASELINE_MODEL_WEIGHTS, char_input_train, phone_input_train, phone_output_train)
+
+
+
+#----------#prediction#-----------------#
+def predict_baseline(input_char_seq, encoder, decoder):
+    state_vectors = encoder.predict(input_char_seq)
+
+    prev_phone = np.zeros((1, 1, PHONE_TOKEN_COUNT))
+    prev_phone[0, 0, phone_to_id[START_PHONE_SYM]] = 1.
+
+    end_found = False
+    pronunciation = ''
+    while not end_found:
+        decoder_output, h, c = decoder.predict([prev_phone] + state_vectors)
+
+        # Predict the phoneme with the highest probability
+        predicted_phone_idx = np.argmax(decoder_output[0, -1, :])
+        predicted_phone = id_to_phone[predicted_phone_idx]
+
+        pronunciation += predicted_phone + ' '
+
+        if predicted_phone == END_PHONE_SYM or len(pronunciation.split()) > MAX_PHONE_SEQ_LEN:
+            end_found = True
+
+        # Setup inputs for next time step
+        prev_phone = np.zeros((1, 1, PHONE_TOKEN_COUNT))
+        prev_phone[0, 0, predicted_phone_idx] = 1.
+        state_vectors = [h, c]
+
+    return pronunciation.strip()
+
+# Helper method for converting vector representations back into words
+def one_hot_matrix_to_word(char_seq):
+    word = ''
+    for char_vec in char_seq[0]:
+        if np.count_nonzero(char_vec) == 0:
+            break
+        hot_bit_idx = np.argmax(char_vec)
+        char = id_to_char[hot_bit_idx]
+        word += char
+    return word
+
+
+# Some words have multiple correct pronunciations
+# If a prediction matches any correct pronunciation, consider it correct.
+def is_correct(word,test_pronunciation):
+    correct_pronuns = phonetic_dict[word]
+    for correct_pronun in correct_pronuns:
+        if test_pronunciation == correct_pronun:
+            return True
+    return False
+
+
+def sample_baseline_predictions(sample_count, word_decoder):
+    sample_indices = random.sample(range(TEST_EXAMPLE_COUNT), sample_count)
+    for example_idx in sample_indices:
+        example_char_seq = char_input_test[example_idx:example_idx+1]
+        predicted_pronun = predict_baseline(example_char_seq, testing_encoder_model, testing_decoder_model)
+        example_word = word_decoder(example_char_seq)
+        pred_is_correct = is_correct(example_word, predicted_pronun)
+        print('✅ ' if pred_is_correct else '❌ ', example_word,'-->', predicted_pronun)
+
+training_model.load_weights(BASELINE_MODEL_WEIGHTS)  # also loads weights for testing models
+sample_baseline_predictions(10, one_hot_matrix_to_word)
+
+"""
+❌  DELOREAN --> D EH2 L ER0 IY1 N AH0
+✅  POYNOR --> P OY1 N ER0
+✅  STRAW --> S T R AO1
+❌  PROCAINE --> P R OW0 K EY1 N IY0
+✅  BATTLESHIP --> B AE1 T AH0 L SH IH2 P
+❌  PRITHVI --> P R IH1 TH IY0
+❌  GRUIS --> G R UW1 Z
+✅  JANK --> JH AE1 NG K
+✅  JON'S --> JH AA1 N Z
+❌  MILISSENT --> M IH1 L IH0 S EH2 N T
+"""
+
+
+#----------#evaluation#-----------------#
+def syllable_count(phonetic_sp):
+    count = 0
+    for phone in phonetic_sp.split():
+        if phone[-1].isdigit():
+            count += 1
+    return count
+
+# Examples:
+for ex_word in list(phonetic_dict.keys())[:3]:
+    print(ex_word, '--', syllable_count(phonetic_dict[ex_word][0]), 'syllables')
+
+
+"""
+SEVERT -- 2 syllables
+BURLATSKY -- 3 syllables
+UNIVA -- 3 syllables
+"""
+
+#----------#bleu score#-----------------#
+
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+
+def is_syllable_count_correct(word, test_pronunciation):
+    correct_pronuns = phonetic_dict[word]
+    for correct_pronun in correct_pronuns:
+        if syllable_count(test_pronunciation) == syllable_count(correct_pronun):
+            return True
+    return False
+
+
+def bleu_score(word, test_pronunciation):
+    references = [pronun.split() for pronun in phonetic_dict[word]]
+    smooth = SmoothingFunction().method1
+    return sentence_bleu(references, test_pronunciation.split(), smoothing_function=smooth)
+
+
+def evaluate(test_examples, encoder, decoder, word_decoder, predictor):
+    correct_syllable_counts = 0
+    perfect_predictions = 0
+    bleu_scores = []
+
+    for example_idx in range(TEST_EXAMPLE_COUNT):
+        example_char_seq = test_examples[example_idx:example_idx + 1]
+        predicted_pronun = predictor(example_char_seq, encoder, decoder)
+        example_word = word_decoder(example_char_seq)
+
+        perfect_predictions += is_correct(example_word, predicted_pronun)
+        correct_syllable_counts += is_syllable_count_correct(example_word, predicted_pronun)
+
+        bleu = bleu_score(example_word, predicted_pronun)
+        bleu_scores.append(bleu)
+
+    syllable_acc = correct_syllable_counts / TEST_EXAMPLE_COUNT
+    perfect_acc = perfect_predictions / TEST_EXAMPLE_COUNT
+    avg_bleu_score = np.mean(bleu_scores)
+
+    return syllable_acc, perfect_acc, avg_bleu_score
+
+
+def print_results(model_name, syllable_acc, perfect_acc, avg_bleu_score):
+    print(model_name)
+    print('-' * 20)
+    print('Syllable Accuracy: %s%%' % round(syllable_acc * 100, 1))
+    print('Perfect Accuracy: %s%%' % round(perfect_acc * 100, 1))
+    print('Bleu Score: %s' % round(avg_bleu_score, 4))
+
+
+syllable_acc, perfect_acc, avg_bleu_score = evaluate(
+    char_input_test, testing_encoder_model, testing_decoder_model, one_hot_matrix_to_word, predict_baseline)
+print_results('Baseline Model',syllable_acc, perfect_acc, avg_bleu_score)
+
+"""
+Baseline Model
+--------------------
+Syllable Accuracy: 92.7%
+Perfect Accuracy: 61.3%
+Bleu Score: 0.7138
+"""
